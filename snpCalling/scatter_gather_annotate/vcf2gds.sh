718b1a5ee4f1163eab3a7a2513c0a507692aabf8
Alan Bergland <aob2x@virginia.edu>
2021-06-19 15:36:20 -0400

    Update vcf2gds.sh

--- snpCalling/scatter_gather_annotate/vcf2gds.sh
+++ snpCalling/scatter_gather_annotate/vcf2gds.sh
@@ -11,8 +11,8 @@
 #SBATCH --account berglandlab
 
 ### sbatch /scratch/aob2x/DEST_freeze1/snpCalling/scatter_gather_annotate/vcf2gds.sh
-### sacct -j 22867936
-### cat /scratch/aob2x/dest/slurmOutput/vcf2gds.22867936
+### sacct -j 22867938
+### cat /scratch/aob2x/dest/slurmOutput/vcf2gds.22867938
 
 module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
 
