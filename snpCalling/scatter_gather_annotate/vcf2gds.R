369fbd9024b49840db5ca5da8515fb3d83ca2faf
Alan Bergland <aob2x@virginia.edu>
2021-06-19 07:10:25 -0400

    optimize

--- snpCalling/scatter_gather_annotate/vcf2gds.R
+++ snpCalling/scatter_gather_annotate/vcf2gds.R
@@ -1,4 +1,4 @@
-#module load intel/18.0 intelmpi/18.0 R/3.6.0; R
+#module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3; R
 
 library(SeqArray)
 
@@ -10,5 +10,7 @@ vcf.fn=args[[1]]
 gds.fn=gsub(".vcf", ".gds", vcf.fn)
 
 #vcf.fn=paste(vcf.fn, ".gz", sep="")
+#vcf.fn="/project/berglandlab/DEST/gds/dest.PoolSeq.SNAPE.NA.NA.10Nov2020.ann.vcf"
+seqParallelSetup(cluster=10, verbose=TRUE)
 
-seqVCF2GDS(vcf.fn, gds.fn, storage.option="ZIP_RA", parallel=10, verbose=T)
+seqVCF2GDS(vcf.fn, gds.fn, storage.option="ZIP_RA", parallel=10, verbose=T, optimize=T)
--- snpCalling/scatter_gather_annotate/vcf2gds.sh
+++ snpCalling/scatter_gather_annotate/vcf2gds.sh
@@ -11,6 +11,8 @@
 #SBATCH --account berglandlab
 
 ### sbatch /scratch/aob2x/DEST_freeze1/snpCalling/scatter_gather_annotate/vcf2gds.sh
+### sacct -j 22867936
+### cat /scratch/aob2x/dest/slurmOutput/vcf2gds.22867936
 
 module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
 
