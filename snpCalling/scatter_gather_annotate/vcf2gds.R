2c2e1467ee43cfc834e841ffd92e548eda1331c8
Alan Bergland <aob2x@virginia.edu>
2021-06-19 07:05:07 -0400

    gds

--- snpCalling/scatter_gather_annotate/vcf2gds.R
+++ snpCalling/scatter_gather_annotate/vcf2gds.R
@@ -9,6 +9,6 @@ args = commandArgs(trailingOnly=TRUE)
 vcf.fn=args[[1]]
 gds.fn=gsub(".vcf", ".gds", vcf.fn)
 
-vcf.fn=paste(vcf.fn, ".gz", sep="")
+#vcf.fn=paste(vcf.fn, ".gz", sep="")
 
-seqVCF2GDS(vcf.fn, gds.fn, storage.option="ZIP_RA")
+seqVCF2GDS(vcf.fn, gds.fn, storage.option="ZIP_RA", parallel=10, verbose=T)
